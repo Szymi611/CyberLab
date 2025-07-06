2751b5ceb8d37e25243189ae2d6842ab4815563c
Szymi611 <szymondomagala32@gmail.com>
2025-07-06 22:23:23 +0200

    Navbar creation with hamburger menu for mobile view

--- frontend/cyberlab/src/App.js
+++ frontend/cyberlab/src/App.js
@@ -1,12 +1,12 @@
 import MatrixBackground from "./components/Background/MatrixBackground.jsx";
+import Navbar from "./components/Navbar/navbar.jsx";
 
 export default function App() {
   return (
     <>
       <MatrixBackground />
-      <div style={{ position: "relative", zIndex: 1, color: "#fff" }}>
-        <h1>Twoja aplikacja</h1>
-      </div>
+      <Navbar />
+
     </>
   );
 }
---
+++ frontend/cyberlab/src/components/Navbar/navbar.jsx
@@ -0,0 +1,51 @@
+import "./navbar.scss";
+import CyberLabLogo from "../../assets/CyberLabLogo.png";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState(false);
+
+  const toggleHamburgerMenu = () => {
+    setIsHamburgerMenuOpen(!isHamburgerMenuOpen);
+  };
+
+  return (
+    <>
+      <nav className="navbar-container">
+        <div className="navbar-logo">
+          <a href="/">
+            <img
+              src={CyberLabLogo}
+              alt="CyberLab logo"
+              className="navbar-logo-image"
+            />
+          </a>
+        </div>
+
+        <div
+          className={`hamburger ${isHamburgerMenuOpen ? "open" : ""}`}
+          onClick={toggleHamburgerMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div className={`navbar-items ${isHamburgerMenuOpen ? "open" : ""}`}>
+          <ul className="navbar-item">
+            <li className="navbar-list-item">
+              <a href="/" className="navbar-link">Home</a>
+            </li>
+            <li className="navbar-list-item">
+              <a href="/" className="navbar-link">About app</a>
+            </li>
+            <li className="navbar-list-item">
+              <a href="/" className="navbar-link">Sign in</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="navbar-underline"></div>
+    </>
+  );
+}
